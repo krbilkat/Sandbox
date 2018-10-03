478c3b5153d938d2d8fe3d61cd5d53d2b07fa5a0
Kato Slava <kkrbilovakk@gmail.com>
2018-10-03 10:42:17 +0200

    Initial Commit

---
+++ Sandbox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sandbox
+//
+//  Created by Katarína Krbilová on 03/10/2018.
+//  Copyright © 2018 Katarína Krbilová. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
